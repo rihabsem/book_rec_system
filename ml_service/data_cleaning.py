cb6b49ad97407b011849585e5a0bff38b88b38a4
rihabsem <semrihab@email.com>
2026-01-28 10:19:24 +0100

    new dataset

--- ml_service/data_cleaning.py
+++ ml_service/data_cleaning.py
@@ -1,29 +1,40 @@
 import pandas as pd
 import re
-
-def clean_text(text):
-    if pd.isnull(text):
+#nombre de lignes 6811
+def text_cleaning(text):
+    if not isinstance(text, str):
         return ""
-    text = str(text).lower()
-    text = re.sub(r"[^a-z\s]", "", text)
+    text = text.lower()
+    text = re.sub(r"[^\w\s]", "", text)
+    text = text.strip()
     return text
-books = pd.read_csv("../data/Books.csv")
+
+books = pd.read_csv("../data/books.csv")
+books.drop(columns=["subtitle", "thumbnail", ])
 print("books read")
-ratings = pd.read_csv("../data/Ratings.csv")
-print("ratings read")
 print(books.head())
-print(ratings.head())
+books = books.drop(columns= "subtitle")
+books = books.drop(columns= "thumbnail")
+print(books.head())
 print(books.info())
-print(ratings.info())
 
-books["Author"] = books["Author"].fillna("")
-#drop duplicates
-books = books.drop_duplicates(subset=["ISBN","Title","Author"])
-ratings = ratings.drop_duplicates(subset=["User-ID","ISBN"])
-list_books = [col for col in books.columns if col != 'Year' and col != 'ISBN']
-for colonne in list_books:
-    books[colonne] = books[colonne].apply(clean_text)
+#traitement des données manquantes
+    #donnée categoriel
+books["authors"] = books["authors"].fillna("Unknown Author")
+books["categories"] = books["categories"].fillna("Uncategorized")
+books["description"] = books["description"].fillna("")
+    #donnée numerique
+books["published_year"] = books["published_year"].fillna(books["published_year"].median())
+books["average_rating"] = books["average_rating"].fillna(books["average_rating"].median())
+books["num_pages"] = books["num_pages"].fillna(books["num_pages"].median())
+books["ratings_count"] = books["ratings_count"].fillna(books["ratings_count"].median())
 
-#saving the documents
+books.info()
+
+# text cleaning
+books['title'] = books['title'].apply(text_cleaning)
+books['authors'] = books['authors'].apply(text_cleaning)
+books['categories'] = books['categories'].apply(text_cleaning)
+books['description'] = books['description'].apply(text_cleaning)
+
 books.to_csv("../data/books_clean.csv",index=False)
-ratings.to_csv("../data/ratings_clean.csv",index=False)
